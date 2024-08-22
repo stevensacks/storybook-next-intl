c543c79c21cfaf95b2e6598488a760a958a87136
Steven Sacks <stevensacks@gmail.com>
2024-08-22 17:23:09 +0900

    fix: dev test [ci skip]

--- .storybook/nextIntl.ts
+++ .storybook/nextIntl.ts
@@ -2,11 +2,11 @@ import en from '../src/stories/messages/en';
 import fr from '../src/stories/messages/fr';
 import ja from '../src/stories/messages/ja';
 
-const messages: Record<string, any> = {en, fr, ja};
+const messagesByLocale: Record<string, any> = {en, fr, ja};
 
 const nextIntl = {
     defaultLocale: 'en',
-    messages,
+    messagesByLocale,
 };
 
 export default nextIntl;
